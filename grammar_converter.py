c184bf86e46af95dc88ee343175cbe6723ef58bf
Ramunechoco <48040337+ramunechoco@users.noreply.github.com>
2019-11-26 13:18:13 +0700

    update

--- grammar_converter.py
+++ grammar_converter.py
@@ -2,8 +2,6 @@ left, right = 0, 1
 
 Terminals, Variables, result = [],[],[]
 
-RULE_DICT = {}
-
 def read_cfg(cfg_file):
     final_grammar = []
     with open(cfg_file) as cfg:
@@ -24,7 +22,7 @@ def isUnitary(rule, variables):
     return False
 
 def isSimple(rule):
-    if len(rule[right]) == 1:
+    if rule[left] in Variables and rule[right][0] in Terminals and len(rule[right]) == 1:
         return True
     return False
 
@@ -45,7 +43,7 @@ def TERM(grammar):
                 for index, value in enumerate(rule[right]):
                     if symbol == value and not symbol in valuestore:
                         valuestore.append(symbol)
-                        newVar = 'S' + str(i)
+                        newVar = 'TERM' + str(i)
                         variablestore.append(newVar)
                         Variables.append(newVar)
                         newGrammar.append([[newVar], [symbol]])
@@ -64,7 +62,7 @@ def BIN(grammar):
         if rlength <= 2:
             newGrammar.append(rule)
         else:
-            newVar = 'B1'
+            newVar = 'BIN'
             Variables.append(newVar)
             newGrammar.append([rule[left],rule[right][0]]+[newVar+'1'])
             i = 1
@@ -72,13 +70,39 @@ def BIN(grammar):
                 var, var2 = newVar+str(i), newVar+str(i+1)
                 Variables.append(var2)
                 newGrammar.append([var, [rule[right][i], var2]])
-            newGrammar.append([newVar+str(rlength-2), rule[right][rlength-2:rlength]]) 
+            newGrammar.append([[newVar+str(rlength-2)], rule[right][rlength-2:rlength]]) 
     return newGrammar
+
+def unit_repeat(grammar):
+    newGrammar = []
+    unit_production = []
+    for rule in grammar:
+        if isUnitary(rule, Variables):
+            unit_production.append([[rule[left]], [rule[right][0]]])
+        else:
+            newGrammar.append(rule)
+        for uni in unit_production:
+            for rule in grammar:
+                if uni[right]==rule[left] and uni[left]!=rule[left]:
+                    newGrammar.append([[uni[left]],[rule[right]]])
+        return newGrammar
+
+def UNIT(grammar):
+    i = 0
+    result = unit_repeat(grammar)
+    tmp = unit_repeat(result)
+    while result != tmp and i < 1000:
+        result = unit_repeat(tmp)
+        tmp = unit_repeat(result)
+        i+=1
+    return result
+
 
 grammar = read_cfg('grammar_placeholder.txt')
 grammar = START(grammar)
 grammar = TERM(grammar)
 grammar = BIN(grammar)
+grammar = UNIT(grammar)
 print("Terminals:")
 print(Terminals)
 print("Variables:")
