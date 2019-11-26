3ca67e4b3660a1fb9aeaa5a2cda5e8d92f7a37bf
mirzaalarsyad <mirza.fathan@gmail.com>
2019-11-26 19:42:15 +0700

    masih sangat sedikit

--- cyk_parser.py
+++ cyk_parser.py
@@ -2,6 +2,25 @@ import os.path
 import argparse
 import grammar_converter
 
+
+left, right = 0, 1
+
+
+sentence = ["lorem", "ipsum", "dolor", "sit", "amet"] #Belom fix, coba gini dulu
+#dianggep grammar sudah didefinisikan
+nTab = len(sentence)*(len(sentence)+1)/2 #bentuk tangga
+table = [[] for i in range nTab]
+
+def Parser(sentence):
+
+    for i in range(nTab):
+        for j in range(len(sentence)):
+            if sentence[j] == grammar[right]:
+                table[i] = 
+
+
+
+'''
 class Node:
     def __init__(self, symbol, child1, child2=None):
         self.symbol = symbol
@@ -10,10 +29,14 @@ class Node:
 
     def __repr__(self):
         return self.symbol
+'''
 
+'''
 class Parser:
 	def __init__(self, grammar, sentence):
 		self.parse_table = None
 		self.prods = {}
 		self.grammar = None
 		self.__call__(sentence)
+    def start()
+'''
