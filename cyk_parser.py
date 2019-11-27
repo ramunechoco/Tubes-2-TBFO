bc09a1bf01fe0603ed02516ede13dfe53feffb29
Muhammad Mirza Fathan Al Arsyad <47689911+mirzaalarsyad@users.noreply.github.com>
2019-11-27 08:34:22 +0700

    hmmm

--- cyk_parser.py
+++ cyk_parser.py
@@ -7,42 +7,72 @@ def loadTXT(txtfile):
     sentence = file.split()
     return sentence
 
+'''
+let the input be a string I consisting of n characters: a1 ... an.
+let the grammar contain r nonterminal symbols R1 ... Rr, with start symbol R1.
+let P[n,n,r] be an array of booleans. Initialize all elements of P to false.
+for each s = 1 to n
+  for each unit production Rv → as
+    set P[1,s,v] = true
+for each l = 2 to n -- Length of span
+  for each s = 1 to n-l+1 -- Start of span
+    for each p = 1 to l-1 -- Partition of span
+      for each production Ra  → Rb Rc
+        if P[p,s,b] and P[l-p,s+p,c] then set P[l,s,a] = true
+if P[n,1,1] is true then
+  I is member of language
+else
+  I is not member of language
+'''
+
 def CYK():
-    P = [[[False for k in range(nt)] for j in range(ns)] for i in range(ns)]
-    for i in range(ns):
+    P = [[[False for k in range(nt+1)] for j in range(ns+1)] for i in range(ns+1)]
+    for i in range(1, ns+1):
         for j in range(ng):
-            print("sentence[", i, "]: ", sentence[i])
-            print("grammar[", j, "][right][0]: ", grammar[j][right][0])
-            print(grammar[j][right][0] == sentence[i])
-            if grammar[j][right][0] == sentence[i]:
+            print(sentence[i-1])
+            if grammar[j][right][0] == sentence[i-1] and len(grammar[j][right]) == 1:
+                print("pass if")
+              #  print("sentence[", i-1, "]: ", sentence[i-1])
+             #   print("grammar[", j, "][right][0]: ", grammar[j][right][0])
+            #    print(grammar[j][right][0] == sentence[i-1])
                 a = grammar[j][left]
+           #     print("grammar[j][left]:", str(a))
                 found = False
                 k = 0
                 while not found and k < nt:
+          #          print("k:", str(k))
                     if variable[k] == a:
                         found = True
                     if not found:
                         k += 1
-                print("k", str(k))
-                P[0][i][k-1] = True
-    for i in range(1, ns):
-        print("i: ", str(i))
-        for j in range(ns-i+1):
-            print("j: ", str(j))
-            for k in range(i-1):
-                print(k)
+         #       print("k:", str(k))
+        #        print("i:", str(i))
+                P[1][i][k+1] = True
+                print("is true")
+       #         print("P[0]" + "[" + str(i) + "][" + str(k+1) + "] = ", P[1][i][k+1]    )
+    for i in range(2, ns+1):
+      #  print("i: ", str(i))
+        for j in range(1, ns-i+2):
+            #print("j: ", str(j))
+            for k in range(1, i):
+                #print("k: ",k)
                 for l in range(ng):
-                    print(l)
+                    #print("l:",l)
                     if len(grammar[l][right]) == 2:
                         a = grammar[l][right][0]
                         found = False
                         m = 0
                         while not found and m < nt:
+                            #print("m:", m)
+                           # print("variable[", str(m), "] = ", variable[m])
+                          #  print("a:",a)
                             if variable[m] == a:
                                 found = True
                             if not found:
                                 m += 1
+                         #       print("m1:", m)
                         b = grammar[l][right][1]
+                        #print("b:", b)
                         found = False
                         n = 0
                         while not found and n < nt:
@@ -51,6 +81,7 @@ def CYK():
                             if not found:
                                 n += 1
                         c = grammar[l][left]
+                        #print("c:",c)
                         found = False
                         o = 0
                         while not found and o < nt:
@@ -58,20 +89,26 @@ def CYK():
                                 found = True
                             if not found:
                                 o += 1
-                        print(m,n,o)
+                        #print(m,n,o)
                         # Vo -> VmVn
-                        if P[k][j][m-1] and P[i-k][j+k][n-1]:
-                            P[i][j][o-1] = True
-    return P[ns-1][0][0]
+                        if m < nt and n < nt and o < nt:
+                            print("passed")
+                            print(P[k][j][m+1])
+                            print(P[i-k][j+k][n+1])
+                            if P[k][j][m+1] and P[i-k][j+k][n+1]:
+                                print("passed again")
+                                P[i][j][o+1] = True
+    return P[ns][1][1]
 
-sentence = loadTXT("newinput.txt")
-grammar = grammar_converter.GetGrammar("input.txt")
-variable = grammar_converter.GetNonTerminals("input.txt")
+sentence = loadTXT("input.txt")
+grammar, variable = grammar_converter.main("grammar_python.txt")
 
 nt = len(variable)
 ng = len(grammar)
 ns = len(sentence) 
 
+print("\n\n")
+print(variable)
 print(nt,ng,ns)
 
 if CYK():
